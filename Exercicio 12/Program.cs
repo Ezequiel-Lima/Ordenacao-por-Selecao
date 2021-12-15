4ab53533ec2e6d77cae3d31633da8656ce4b110e
Ezequiel-Lima <joao.lima94@fatec.sp.gov.br>
2021-12-15 09:35:57 -0300

    Add project files.

---
+++ Exercicio 12/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = { 7, 5, 6, 7, 8, 9, 10, 2 };
+            OrdenacaoPorSelecao(vetor);
+
+            Console.WriteLine("Ordenação Por Seleção: ");
+            foreach (int item in vetor)
+            {
+                Console.WriteLine(item);
+            }
+            
+            Console.ReadKey();
+        }
+
+        static int[] OrdenacaoPorSelecao(int[] vetor)
+        {
+            int min, aux;
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                min = i;
+
+                for (int j = i + 1; j < vetor.Length; j++)
+                {
+                    if (vetor[j] < vetor[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                if (min != i)
+                {
+                    aux = vetor[min];
+                    vetor[min] = vetor[i];
+                    vetor[i] = aux;
+                }
+            }
+
+            return vetor;
+        }
+    }
+}
